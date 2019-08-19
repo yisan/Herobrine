4c773a4c6359050979b0e8b8efbb3a31faca2bdf
ShenMian <shenmian000@outlook.com>
2019-08-20 01:57:26 +0800

    Update

--- Himconsole/type.h
+++ Himconsole/type.h
@@ -1,20 +1,26 @@
 // Copyright 2019 SMS
 // License(GPL)
 // Author: ShenMian
+// 声明/定义各种类型
 
 #ifndef TYPE_H_
 #define TYPE_H_
 
-#include <string>
-#include <vector>
-#include <deque>
 
+#include <string>
 using std::string;
+using std::to_string;
+
+#include <vector>
 using std::vector;
+
+#include <deque>
 using std::deque;
+
 
 typedef unsigned char		uchar;
 typedef unsigned short	ushort;
 typedef unsigned int		uint;
+
 
 #endif	// TYPE_H_
